7259b7b9d4f252ba0c2ca9f7f2df3faa37bed2c1
Gbenga Ojo-Aromokudu <gojoaromokudu@gmail.com>
2020-02-21 19:15:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,29 +11,29 @@ array.push(5)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+array.unshift(6)  
 end
 
 def remove_element_from_end_of_array(array)
-  
+array.pop  
 end
 
 def remove_element_from_start_of_array(array)
-  
+array.unshift  
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+array[1]  
 end
 
 def retrieve_first_element_from_array(array)
-  
+array[0]  
 end
 
 def retrieve_last_element_from_array(array)
-  
+array[-1]  
 end
 
 def update_element_from_index(array, index_number, element)
-
+array[4] = "hey"
 end
